15b70be7c60275499f151822f1db3d7462b3725b
John Gibson <jg20019@gmail.com>
2021-09-13 17:54:22 -0400

    Add basic styling to Login Form

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -1,13 +1,17 @@
 
 export default function LoginForm(props) {
   return (
-    <form method="POST" action="/login">
+    <form class="uk-form-stacked" method="POST" action="/login">
       <p>{props.error}</p>
-      <label> Username </label>
-      <input type="text" name="username" /> 
-      <label> Password </label>
-      <input type="text" name="password" />
-      <input type="submit" value="login" /> 
+      <div>
+        <label class="uk-form-label"> Username </label>
+        <input class="uk-form-width-large uk-input" type="text" name="username" /> 
+      </div>
+      <div>
+        <label class="uk-form-label"> Password </label>
+        <input class="uk-form-width-large uk-input" type="text" name="password" />
+      </div>
+      <input class="uk-button uk-button-primary" type="submit" value="login" /> 
     </form>
   );
 }
