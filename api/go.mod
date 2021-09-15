47929a1ba7d97025d28bb2dd79ac36d1414289d9
John Gibson <jg20019@gmail.com>
2021-09-15 16:14:04 -0400

    Refactor database code to its own folder

--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,9 @@ module example.com/wwtl-app
 go 1.16
 
 require (
+	example.com/database v0.0.0-00010101000000-000000000000
 	github.com/gin-gonic/gin v1.7.4
-	github.com/mattn/go-sqlite3 v1.14.8
+	github.com/mattn/go-sqlite3 v1.14.8 // indirect
 )
+
+replace example.com/database => ./database
--- api/main.go
+++ api/main.go
@@ -1,33 +1,58 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	_ "github.com/mattn/go-sqlite3"
-	"log"
 	"net/http"
 	"strconv"
+	"example.com/database"
 )
 
-var db *sql.DB
 
-type HistoryRecord struct {
-	ID             int64   `json:"id"`
-	Date           string  `json:"date"`
-	Title          string  `json:"title"`
-	Content        string  `json:"content"`
-	Origin         string  `json:"origin"`
-	Author         string  `json:"author"`
-	SourceArchive  *string `json:"sourceArchive"`
-	AttachmentType *string `json:"attachmentType"`
-	FileName       *string `json:"fileName"`
-	RecordType     *string `json:"recordType"`
-	Collections    *string `json:"collections"`
+
+type Login struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
+type LoginResult struct {
+	Succeeded bool `json:"succeeded"`
+	ErrorMessage string `json:"errorMessage"`
+}
+
+func authenticate(login Login) bool {
+	// TODO: 
+	// need to hash password, get user and then check that the
+	// has stored in the database matches up with the hash stored
+	// in the databse
+	return login.Username == "admin" && login.Password == "password"
+}
+
+func loginHandler(c *gin.Context) {
+	var json Login
+	var result LoginResult
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H {"error": "invalid username/password"})
+		return 
+	}
+
+
+	if authenticate(json) {
+		result.Succeeded = true
+
+		c.SetCookie("username", json.Username, 3600, "/", "", true, true)
+		c.IndentedJSON(http.StatusOK, result)
+	} else {
+		result.Succeeded = false
+		result.ErrorMessage = "invalid username/password"
+		c.IndentedJSON(http.StatusOK, result)
+	}
+}
+
+
 func getRecords(c *gin.Context) {
-	records, err := publishedHistoryRecords(0)
+	records, err := database.PublishedHistoryRecords(0)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 		c.IndentedJSON(http.StatusOK, records)
@@ -42,7 +67,7 @@ func getRecordByID(c *gin.Context) {
 		return
 	}
 
-	record, err := historyRecordByID(id)
+	record, err := database.HistoryRecordByID(id)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 		c.IndentedJSON(http.StatusOK, record)
@@ -52,19 +77,7 @@ func getRecordByID(c *gin.Context) {
 }
 
 func main() {
-	dsn := "file:archive.db"
-
-	var err error
-	db, err = sql.Open("sqlite3", dsn)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pingErr := db.Ping()
-	if pingErr != nil {
-		log.Fatal(pingErr)
-	}
+	database.Connect()
 
 	fmt.Println("Running Williamsport-Washington Township Public Library - History Database")
 	fmt.Println("Server is listening on localhost:8080")
@@ -74,153 +87,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/records", getRecords)
 	router.GET("/records/:id", getRecordByID)
+	router.POST("/login", loginHandler)
 	router.Run("localhost:8080")
 }
 
-func allHistoryRecords(offset int) ([]HistoryRecord, error) {
-	var records []HistoryRecord
-
-	rows, err := db.Query(`
-  SELECT id, date, title
-  FROM history_record
-  WHERE deleted_at IS NULL
-    AND record_status_id = 3
-  ORDER BY date(date), title ASC
-  LIMIT 50
-  OFFSET ?
-  ;`, offset)
-
-	if err != nil {
-		return nil, fmt.Errorf("allHistoryRecords: %v", err)
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		var record HistoryRecord
-		if err := rows.Scan(&record.ID, &record.Date, &record.Title); err != nil {
-			return nil, fmt.Errorf("allHistoryRecords: %v", err)
-		}
-		records = append(records, record)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("allHistoryRecords: %v", err)
-	}
-
-	return records, nil
-}
-
-func publishedHistoryRecords(offset int) ([]HistoryRecord, error) {
-
-	var records []HistoryRecord
-
-	rows, err := db.Query(`
-  SELECT
-    history_record.id,
-    history_record.date,
-    history_record.title,
-    history_record.content,
-    history_record.origin,
-    history_record.author,
-    source_archive.name,
-    attachment_type_name,
-    file_name,
-    record_type.name,
-    (SELECT GROUP_CONCAT(collection.name, ';')
-     FROM history_record hr
-     LEFT OUTER JOIN record_collections ON hr.id = record_collections.record_id
-     LEFT OUTER JOIN collection ON record_collections.collection_id = collection.id
-     WHERE history_record.id = hr.id) AS collections  
-  FROM history_record
-  LEFT OUTER JOIN (
-    SELECT record_id as file_attachment_record_id, file_name, attachment_type.name as attachment_type_name
-    FROM file_attachment
-    INNER JOIN attachment_type
-    ON attachment_type.id = attachment_type_id
-    ) ON history_record.id = file_attachment_record_id
-  LEFT OUTER JOIN record_type ON history_record.record_type_id = record_type.id
-  LEFT OUTER JOIN source_archive ON history_record.source_archive_id = source_archive.id
-  WHERE record_status_id = 3
-  AND deleted_at IS NULL
-  ORDER BY date(history_record.date), history_record.title
-  LIMIT 50 OFFSET 0
-  `, offset)
-
-	if err != nil {
-		return nil, fmt.Errorf("publishedHistoryRecords: %v", err)
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		var record HistoryRecord
-		err := rows.Scan(
-			&record.ID,
-			&record.Date,
-			&record.Title,
-			&record.Content,
-			&record.Origin,
-			&record.Author,
-			&record.SourceArchive,
-			&record.AttachmentType,
-			&record.FileName,
-			&record.RecordType,
-      &record.Collections)
-		if err != nil {
-			return nil, fmt.Errorf("publishedHistoryRecords: %v", err)
-		}
-		records = append(records, record)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("publishedHistoryRecords: %v", err)
-	}
-
-	return records, nil
-}
-
-func historyRecordByID(id int64) (HistoryRecord, error) {
-	var record HistoryRecord
-
-	row := db.QueryRow(`
-  SELECT
-    history_record.id,
-    history_record.date,
-    history_record.title,
-    history_record.content,
-    history_record.origin,
-    history_record.author,
-    source_archive.name,
-    attachment_type_name,
-    file_name,
-    record_type.name, 
-    GROUP_CONCAT(collection.name, ';') AS collections
-  FROM history_record
-  LEFT OUTER JOIN record_collections ON record_collections.record_id = history_record.id
-  LEFT OUTER JOIN collection ON record_collections.collection_id = collection.id
-  LEFT OUTER JOIN (
-    SELECT record_id as file_attachment_record_id, file_name, attachment_type.name as attachment_type_name
-    FROM file_attachment
-    INNER JOIN attachment_type
-    ON attachment_type.id = attachment_type_id
-  ) ON history_record.id = file_attachment_record_id
-  LEFT OUTER JOIN record_type ON history_record.record_type_id = record_type.id
-  LEFT OUTER JOIN source_archive ON history_record.source_archive_id = source_archive.id
-  WHERE record_status_id = 3
-  AND deleted_at IS NULL
-  AND history_record.id = ?;
-  `, id)
-
-	err := row.Scan(&record.ID, &record.Date, &record.Title, &record.Content, &record.Origin,
-		&record.Author, &record.SourceArchive, &record.AttachmentType, &record.FileName, &record.RecordType,
-		&record.Collections)
-
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return record, fmt.Errorf("historyRecordById %d: no such record", id)
-		}
-		return record, fmt.Errorf("historyRecordById %d: %v", id, err)
-	}
-	return record, nil
-}
