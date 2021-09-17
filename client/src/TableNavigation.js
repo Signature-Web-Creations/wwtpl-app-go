a4bf1a38db3091fb1c4bd0d0e010b12da5f5fbcd
John Gibson <jg20019@gmail.com>
2021-09-17 14:04:58 -0400

    Add TableNavigation

---
+++ client/src/TableNavigation.js
@@ -0,0 +1,13 @@
+
+
+export default function TableNavigation(props) {
+  const pages = props.pages
+  console.log('Pages: ', pages)
+  if (pages === null) {
+  } else {
+    return (
+      <nav>
+      </nav>
+    )
+  }
+}
