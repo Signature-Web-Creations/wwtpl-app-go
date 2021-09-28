f67af2d14744d4c9471e62379f20f0a1f2b430c6
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-09-28 16:22:19 -0400

    Add ability to clear search parameters

--- client/src/SearchForm.js
+++ client/src/SearchForm.js
@@ -1,85 +1,160 @@
 import './SearchForm.css'
-import {useState} from 'react';
+import { useState } from 'react'
 
 export default function SearchForm(props) {
-  const [query, setQuery] = useState("")
-  const [searchYear, setSearchYear] = useState("")
-  const [searchCollection, setSearchCollection] = useState("")
-  const [searchSourceArchive, setSearchSourceArchive] = useState("")
-  const [searchRecordType, setSearchRecordType] = useState("")
+  const [query, setQuery] = useState('')
+  const [searchYear, setSearchYear] = useState('')
+  const [searchCollection, setSearchCollection] = useState('')
+  const [searchSourceArchive, setSearchSourceArchive] = useState('')
+  const [searchRecordType, setSearchRecordType] = useState('')
 
   const [showAdvancedSearch, setShowAdvancedSearch] = useState(false)
-
-  const show = showAdvancedSearch ? "show" : "" 
+  const performSearch = () => {
+    const params = {
+      query,
+      searchYear,
+      searchCollection,
+      searchSourceArchive,
+      searchRecordType,
+    }
+    props.onSubmit(params)
+  }
+  const clearSearchFields = () => {
+    setQuery('')
+    setSearchYear('')
+    setSearchCollection('')
+    setSearchSourceArchive('')
+    setSearchRecordType('')
+  }
+  const show = showAdvancedSearch ? 'show' : ''
   const advancedSearchClassName = `advancedSearchOptions uk-grid-small ${show}`
+
   return (
-    <form className="uk-margin-top" onSubmit={(e) => {
-      e.preventDefault()
-      const params = {query, searchYear, searchCollection, searchSourceArchive, searchRecordType}
-      props.onSubmit(params)
-    }}>
-        <div className="searchBar uk-grid-small" uk-grid="true">
-          <div className="uk-width-expand@s">
-            <input className="uk-input" type="text" placeholder="Search" onChange={(e) => {
+    <form
+      className="uk-margin-top"
+      onSubmit={(e) => {
+        e.preventDefault()
+        performSearch()
+      }}
+    >
+      <div className="searchBar uk-grid-small" uk-grid="true">
+        <div className="uk-width-expand@s">
+          <input
+            className="uk-input"
+            value={query}
+            type="text"
+            placeholder="Search"
+            onChange={(e) => {
               setQuery(e.target.value)
-            }} />
-          </div>
-          <div className="uk-width-1-3@s">
-            <select className="uk-select" onChange={(e) => {
+            }}
+          />
+        </div>
+        <div className="uk-width-1-3@s">
+          <select
+            className="uk-select"
+            value={searchYear}
+            onChange={(e) => {
               setSearchYear(e.target.value)
-            }}>
-              <option value="">Year</option>
-              { props.years.map(year => <option key={year} value={year}> {year} </option>)}
-            </select>
+            }}
+          >
+            <option value="">Year</option>
+            {props.years.map((year) => (
+              <option key={year} value={year}>
+                {' '}
+                {year}{' '}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div
+          className="advancedSearchButton uk-width-auto@s uk-grid-small"
+          uk-grid="true"
+        >
+          <div className="uk-width-auto">
+            <button
+              className="uk-button uk-button-default"
+              uk-icon="more"
+              uk-tooltip="Advanced Search Options"
+              onClick={() => {
+                setShowAdvancedSearch(!showAdvancedSearch)
+              }}
+            ></button>
           </div>
-          <div className="advancedSearchButton uk-width-auto@s uk-grid-small" uk-grid="true">
-            <div className="uk-width-auto">
-              <button 
-                className="uk-button uk-button-default"
-                uk-icon="more"
-                uk-tooltip="Advanced Search Options"
-                onClick={() => {
-                  setShowAdvancedSearch(!showAdvancedSearch)
-                }}
-              ></button>
-            </div>
-            <div className="uk-width-auto">
-              <button 
-                className="uk-button uk-button-primary searchButton"
-                type="submit"
-                uk-icon="search"
-                uk-tooltip="Search">
-              </button>
-            </div>   
+          <div className="uk-width-auto">
+            <button
+              className="uk-button uk-button-default"
+              type="submit"
+              uk-icon="search"
+              uk-tooltip="Clear Search"
+              onClick={() => {
+                clearSearchFields()
+              }}
+            ></button>
+          </div>
+          <div className="uk-width-auto">
+            <button
+              className="uk-button uk-button-primary searchButton"
+              type="submit"
+              uk-icon="search"
+              uk-tooltip="Search"
+            ></button>
           </div>
         </div>
-        
-      <div className={advancedSearchClassName} uk-grid="true"> 
-          <div className="uk-width-1-3@s">
-            <select className="uk-select" onChange={(e) => {
+      </div>
+
+      <div className={advancedSearchClassName} uk-grid="true">
+        <div className="uk-width-1-3@s">
+          <select
+            className="uk-select"
+            value={searchRecordType}
+            onChange={(e) => {
               setSearchRecordType(e.target.value)
-            }}>
-              <option value=""> Type </option>
-              { props.recordTypes.map(({id, name}) => <option key={id} value={id}> {name} </option>) }
-            </select>
-          </div>
-          <div className="uk-width-1-3@s">
-            <select className="uk-select" onChange={(e) => {
-              setSearchSourceArchive(e.target.value) 
-            }}>
-              <option value=""> Source Archive </option>
-              { props.sourceArchives.map(({id, name}) => <option key={id} value={id}> {name} </option>) }
-            </select>
-          </div>
-          <div className="uk-width-1-3@s">
-            <select className="uk-select" onChange={(e) => {
-              setSearchCollection(e.target.value)
-            }}>
-              <option value=""> Collection </option>
-              { props.collections.map(({id, name}) => <option key={id} value={id}> {name} </option>) }
-            </select>
-          </div>
+            }}
+          >
+            <option value=""> Type </option>
+            {props.recordTypes.map(({ id, name }) => (
+              <option key={id} value={id}>
+                {' '}
+                {name}{' '}
+              </option>
+            ))}
+          </select>
         </div>
-      </form>
-  );
+        <div className="uk-width-1-3@s">
+          <select
+            className="uk-select"
+            value={searchSourceArchive}
+            onChange={(e) => {
+              setSearchSourceArchive(e.target.value)
+            }}
+          >
+            <option value=""> Source Archive </option>
+            {props.sourceArchives.map(({ id, name }) => (
+              <option key={id} value={id}>
+                {' '}
+                {name}{' '}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="uk-width-1-3@s">
+          <select
+            className="uk-select"
+            value={searchCollection}
+            onChange={(e) => {
+              setSearchCollection(e.target.value)
+            }}
+          >
+            <option value=""> Collection </option>
+            {props.collections.map(({ id, name }) => (
+              <option key={id} value={id}>
+                {' '}
+                {name}{' '}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </form>
+  )
 }
