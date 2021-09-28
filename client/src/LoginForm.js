5e9ed3951c80ebd4a726ebbc7b560bbfe2a2481b
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-09-28 16:42:47 -0400

    Update login form styles

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -1,13 +1,12 @@
-import {useState} from 'react';
-import {useAuth} from './auth.js';
-import {Redirect} from 'react-router-dom';
-
+import { useState } from 'react'
+import { useAuth } from './auth.js'
+import { Redirect } from 'react-router-dom'
 
 export default function LoginForm(props) {
   const auth = useAuth()
 
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("") 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   const [error, setError] = useState(null)
 
   const changeUsername = (event) => {
@@ -25,25 +24,60 @@ export default function LoginForm(props) {
     auth.signin(username, password, () => setError)
   }
 
+  const handleCloseErrorBox = () => {
+    setError(null)
+  }
+
+  function ErrorBox() {
+    return (
+      <div uk-alert="true" class="uk-alert-danger">
+        <span
+          className="uk-alert-close"
+          uk-close="true"
+          onClick={handleCloseErrorBox}
+        ></span>
+        <p>[{error}]</p>
+      </div>
+    )
+  }
+
   console.log(auth.user)
   if (auth.user) {
     return <Redirect to="/" />
   }
 
   return (
-    <form className="uk-form-stacked" onSubmit={handleSubmit}>
-      <p>{error}</p>
+    <form
+      className="uk-form-stacked uk-form-width-large uk-margin-top"
+      onSubmit={handleSubmit}
+    >
+      {error && <ErrorBox />}
+
       <div>
         <label className="uk-form-label"> Username </label>
-        <input className="uk-form-width-large uk-input" type="text" name="username"
-               onChange={changeUsername} value={username} /> 
+        <input
+          className="uk-form-width-large uk-input"
+          type="text"
+          name="username"
+          onChange={changeUsername}
+          value={username}
+        />
       </div>
       <div>
         <label className="uk-form-label"> Password </label>
-        <input className="uk-form-width-large uk-input" type="password" name="password"
-               onChange={changePassword} value={password} />
+        <input
+          className="uk-form-width-large uk-input"
+          type="password"
+          name="password"
+          onChange={changePassword}
+          value={password}
+        />
       </div>
-      <input className="uk-button uk-button-primary" type="submit" value="login" /> 
+      <input
+        className="uk-button uk-button-primary uk-margin-top"
+        type="submit"
+        value="login"
+      />
     </form>
-  );
+  )
 }
