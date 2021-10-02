cd222f6f34fe98a9b745562df687cc7a94e5cb3e
John Gibson <jg20019@gmail.com>
2021-10-02 04:27:50 -0400

    Add to the RecordForm

--- client/src/admin/RecordForm.js
+++ client/src/admin/RecordForm.js
@@ -2,12 +2,21 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 // import { getPublicRecordByID } from '../api'
 
+
 import MessageBox from '../MessageBox'
 
 function RecordForm(props) {
   let { id } = useParams()
 
   const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const [date, setDate] = useState('')
+  const [origin, setOrigin] = useState('')
+  const [author, setAuthor] = useState('')
+
+  const [recordType, setRecordType] = useState('');
+  const [sourceArchive, setSourceArchive] = useState(''); 
+  const [collections, setCollections] = useState([])
 
   const newRecord = id === undefined
 
@@ -20,8 +29,28 @@ function RecordForm(props) {
     }
   }, [newRecord, id])
 
+  const handleCollection = (id) => {
+    if (collections.includes(id)) {
+      setCollections(collections.filter(i => i != id))
+    } else {
+      setCollections(collections.concat(id))
+    }
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
+    const recordData = {
+      title,
+      content, 
+      date, 
+      origin,
+      author,
+      recordType, 
+      sourceArchive,
+      collections 
+    }
+
+    console.log(recordData)
   }
 
   const handleCloseBox = (message) => {
@@ -42,9 +71,91 @@ function RecordForm(props) {
       <h1> {header} </h1> 
 
       <div> 
-        <label> Title </label>
-        <input type="text" value={title} onChange={setTitle} />
+        <label className="uk-form-label"> Title </label>
+        <input
+          className="uk-form-width-large uk-input"
+          type="text"
+          value={title}
+          onChange={(e) => { setTitle(e.target.value) } } />
       </div> 
+
+      <div> 
+        <label className="uk-form-label"> Content </label>
+        <textarea 
+          className="uk-form-width-large uk-textarea"
+          value={content}
+          onChange={(e) => { setContent(e.target.value) } } />
+      </div> 
+
+      <div> 
+        <label className="uk-form-label"> Date </label>
+        <input 
+          className="uk-form-width-large uk-input"
+          type="text"
+          value={date}
+          onChange={(e) => { setDate(e.target.value) } } />
+      </div> 
+
+
+      <div> 
+        <label className="uk-form-label"> Origin </label>
+        <input 
+          className="uk-form-width-large uk-input"
+          type="text" 
+          value={origin}
+          onChange={(e) => { setOrigin(e.target.value) } } />
+      </div> 
+
+      <div> 
+        <label className="uk-form-label"> Author </label>
+        <input
+          className="uk-form-width-large uk-input"
+          type="text"
+          value={author} 
+          onChange={(e) => { setAuthor(e.target.value) } } />
+      </div> 
+
+      <div> 
+        <label className="uk-form-label"> Record Type </label>
+        <select 
+          className="uk-select uk-form-width-large" 
+          value={recordType}
+          onChange={(e) => {setRecordType(e.target.value)}}>
+          <option value=''> No Record Type </option> 
+          { props.recordTypes.map(({id, name}) => <option key={id} value={id}> {name} </option>) }
+        </select>
+      </div> 
+
+      <div> 
+        <label className="uk-form-label"> Source Archive </label>
+        <select
+          className="uk-select uk-form-width-large"
+          value={sourceArchive}
+          onChange={(e) => {setSourceArchive(e.target.value)}}>
+          <option value=''> No Source Archive </option> 
+          { props.sourceArchives.map(({id, name}) => <option key={id} value={id}> {name} </option>) }
+        </select>
+      </div> 
+
+      <div> 
+        <label className="uk-form-label"> Collections </label>
+        {props.collections.map(({id, name}) => {
+          return (
+            <label className="uk-form-label" key={id} >
+              <input 
+                className="uk-checkbox"
+                type="checkbox"
+                key={id}
+                value={id}
+                checked={collections.includes(id)}
+                onChange={() => {handleCollection(id)}} />
+            &nbsp;&nbsp;  { name } 
+            </label>
+          )
+        })}
+      </div>
+
+      
 
       <input
         className="uk-button uk-button-primary uk-margin-top"
@@ -52,8 +163,6 @@ function RecordForm(props) {
         value="Add Record"
       />
     </form>
-    )
-}
-
+    ) } 
 
 export default RecordForm;
