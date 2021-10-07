e6c56013f4f4dd74dcbb131172a6e4c112a081fa
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-10-07 17:06:17 -0400

    update message box to have a warning or notice message

--- client/src/MessageBox.js
+++ client/src/MessageBox.js
@@ -2,17 +2,21 @@ function MessageBox(props) {
   let messageClass = ''
   if (props.type === 'error') {
     messageClass = 'uk-alert-danger'
+  } else if (props.type === 'warning') {
+    messageClass = 'uk-alert-warning'
   } else {
     messageClass = 'uk-alert-success'
   }
 
   return (
     <div uk-alert="true" className={messageClass}>
-      <button
-        className="uk-alert-close"
-        uk-close="true"
-        onClick={props.onClick}
-      ></button>
+      {props.type !== 'warning' && (
+        <button
+          className="uk-alert-close"
+          uk-close="true"
+          onClick={props.onClick}
+        ></button>
+      )}
       <p>{props.message}</p>
     </div>
   )
