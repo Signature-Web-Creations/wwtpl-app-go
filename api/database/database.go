2bbd5ea1c058cd0ca82633898329e030e54d9d11
John Gibson <jg20019@gmail.com>
2021-10-21 07:39:44 -0400

    Remove unfinished code

--- api/database/database.go
+++ api/database/database.go
@@ -821,37 +821,3 @@ func RestoreRecord(recordID int64) error {
 	return nil
 }
 
-func UpdatePageViewCount() error {
-	tx, err := db.Begin()
-
-	_, err = tx.QueryRow(`
-	 SELECT FROM page_views
-	 WHERE date = date('now')
-	`, recordId)
-
-	if err != nil {
-		tx.Rollback()
-		return fmt.Errorf("Error deleting record_collections: %v", err)
-	}
-
-	query := sq.Insert("record_collections")
-	query = query.Columns("record_id", "collection_id")
-	for _, collectionId := range record.Collections {
-		query = query.Values(recordId, collectionId)
-	}
-
-	sql, arguments, err := query.ToSql()
-	if err != nil {
-		tx.Rollback()
-		return fmt.Errorf("Error creating query for collections: %v", err)
-	}
-
-	_, err = tx.Exec(sql, arguments...)
-
-	
-	if err != nil {
-		tx.Rollback()
-		return fmt.Errorf("Error inserting collections: %v", err)
-	}
-
-}
