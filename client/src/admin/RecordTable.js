a989e3b15e77409d20353af76fe2b5ce1aa8839e
John Gibson <jg20019@gmial.com>
2021-11-15 14:45:43 -0500

    Fix misaligned headers and columns in admin record table

--- client/src/admin/RecordTable.js
+++ client/src/admin/RecordTable.js
@@ -1,17 +1,19 @@
 import RecordRow from './RecordRow'
+import {useAuth} from '../auth.js'
 
 function NoResultsPage() {
   return <h1> Your search did not return any results. </h1>
 }
 
 function AdminListings(props) {
+  const auth = useAuth();
   return (
     <table className="uk-table uk-table-middle uk-table-divider uk-table-hover uk-margin-medium">
       <thead>
         <tr>
-          <th className="uk-table-small"></th>
-          {props.user !== undefined && props.user.role === 'admin' && (
-            <th className="uk-table-small"></th>
+          <th className="uk-width-small"></th>
+          {auth.user.role === 'admin' && (
+            <th className="uk-width-small"></th>
           )}
           <th className="uk-width-small">Date</th>
           <th>Title</th>
